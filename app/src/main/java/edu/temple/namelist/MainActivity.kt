d31535495ed96f38bb9a8b1f753401a9f38cc962
Kevin Nguyen <tuo17215@temple.edu>
2025-10-15 15:25:30 -0400

    Merge into master branch

--- app/src/main/java/edu/temple/namelist/MainActivity.kt
+++ app/src/main/java/edu/temple/namelist/MainActivity.kt
@@ -16,7 +16,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         names = mutableListOf("Kevin Shaply", "Stacey Lou", "Gerard Clear", "Michael Studdard", "Michelle Studdard")
 
         val spinner = findViewById<Spinner>(R.id.spinner)
